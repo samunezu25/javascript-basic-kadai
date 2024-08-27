24dcecfbd0e441051fdbf8086f0b5643ab29f436
samunezu25 <164389113+samunezu25@users.noreply.github.com>
2024-08-27 22:27:06 +0900

    訂正いたしました。ご確認よろしくお願い致します。

--- Kadai_005/bmi.js
+++ Kadai_005/bmi.js
@@ -1,6 +1,8 @@
-//定数の宣言・値の代入
+//変数の宣言・値の代入
 const height = 1.7; 
 const weight = 68; 
+const bmi = (weight/(height*height));
+
 
 //コンソールへの出力
- console.log(weight/(height*height));
+ console.log(bmi)
